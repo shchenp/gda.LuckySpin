b959d3c497f1b8ec30a7447930b837b2779c2dbd
Nikita <np.shche@gmail.com>
2025-03-19 00:04:30 +0300

    added animation for rotation token

--- Assets/Scripts/Extensions/LuckySpinScreen.cs
+++ Assets/Scripts/Extensions/LuckySpinScreen.cs
@@ -1,3 +1,4 @@
+using LuckySpin;
 using TMPro;
 using UnityEngine;
 
--- Assets/Scripts/LuckySpin/Token.cs
+++ Assets/Scripts/LuckySpin/Token.cs
@@ -1,21 +1,40 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Token : MonoBehaviour
+namespace LuckySpin
 {
-   public event Action<int> CountChanged;
+   public class Token : MonoBehaviour
+   {
+      public UnityEvent TokensOut; 
+      public event Action<int> CountChanged;
    
-   [SerializeField] private int _count;
+      [SerializeField] private int _count;
 
-   private void Start()
-   {
-      CountChanged?.Invoke(_count);
-   }
+      private void Start()
+      {
+         CountChanged?.Invoke(_count);
 
-   public void GetToken()
-   {
-      _count--;
-      
-      CountChanged?.Invoke(_count);
+         OnTokensDepleted();
+      }
+
+      private void OnTokensDepleted()
+      {
+         if (_count == 0)
+         {
+            TokensOut?.Invoke();
+         }
+      }
+
+      public void GetToken()
+      {
+         if (_count > 0)
+         {
+            _count--;
+            CountChanged?.Invoke(_count);
+         }
+
+         OnTokensDepleted();
+      }
    }
 }
